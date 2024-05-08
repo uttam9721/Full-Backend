fa2a893a8e3ef03f1749c53d1942d394b4b8eef8
Uttam Kumar <uttammaurya377@gmail.com>
2024-05-09 00:52:08 +0530

    updated

--- Backend_6/app.js
+++ Backend_6/app.js
@@ -3,7 +3,9 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
-
+const bcrypt = require('bcrypt');
+const jwt =require('jsonwebtoken');
+const cookie= require('cookie');
 
 app.use(cookieParser());
 
@@ -18,4 +20,41 @@ app.get("/read",(req,res)=>{
     res.send("done cookie")
 })
 
-app.listen(3000);
+// How to use bcrypt for password encryption or decryption 
+//$2b$10$2bNXZe/MhZTcWElIElFPMOrCL6Ad5M/vizl9SgShhad.G0UAhQGgy
+// password matched
+
+
+app.get("/hash",(req,res)=>{
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash("myPlaintextPassword", salt, function(err, hash) {
+           console.log(hash);
+        });
+    });
+})
+
+app.get("/dec",(req,res)=>{
+    bcrypt.compare("myPlaintextPassword", "$2b$10$2bNXZe/MhZTcWElIElFPMOrCL6Ad5M/vizl9SgShhad.G0UAhQGgy", function(err, result) {
+        console.log(result);
+});
+});
+
+//How to work jwt 
+//JSON Web Tokens (JWT) are an open standard, which is defined in JSON Web Token (JWT) Specification RFC 7519. They securely represent claims between two parties.
+
+app.get("/jwt",(req,res)=>{
+  let token=  jwt.sign({email:"uttam7@gmail.com"},"secret");
+  res.cookie("token",token);
+  res.send("done");
+})
+
+
+app.get("/token",(req,res)=>{
+  let data=  jwt.verify(req.cookies.token,"secret");
+    console.log(data);
+})
+
+
+app.listen(3000,()=>{
+    console.log("listening on port 3000");
+});
